b944d9bc0ae0a92061434a0da59f9b0d2630e52e
cooper-mj <coopermj@stanford.edu>
2021-08-28 16:46:28 -0700

    Style updates, ConvNetJS hyperlink opens in new tab

--- components/classifier_demo/demo.js
+++ components/classifier_demo/demo.js
@@ -5,7 +5,7 @@ import { faInfoCircle } from '@fortawesome/free-solid-svg-icons'
 import { bottomTooltip } from '../../helpers';
 
 export default function ConvNetDemo() {
-  var demo_html_raw = "<!DOCTYPE html><html lang='en'><head><link rel='stylesheet' type='text/css' href='style.css' /><script src='convnet_demo/convnet-min.js'></script><script src='convnet_demo/convnet_demo.js'></script><script>function page_loaded() {$('#convask').click(function(){$('#explain').slideToggle('fast');})start_convnet_demo();}</script></head><body onload='page_loaded()'><div id='teaser'><div id='convnetvis'></div><div id='demomsg'>*This network is running <a href='https://cs.stanford.edu/people/karpathy/convnetjs/' id='darkLink'>live in your browser</a>.</div></div><!-- jQuery and Boostrap --><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script></body></html>";
+  var demo_html_raw = "<!DOCTYPE html><html lang='en'><head><link rel='stylesheet' type='text/css' href='style.css' /><script src='convnet_demo/convnet-min.js'></script><script src='convnet_demo/convnet_demo.js'></script><script>function page_loaded() {$('#convask').click(function(){$('#explain').slideToggle('fast');})start_convnet_demo();}</script></head><body onload='page_loaded()'><div id='teaser'><div id='convnetvis'></div><div id='demomsg'>*This network is running <a href='https://cs.stanford.edu/people/karpathy/convnetjs/' target='_blank'>live in your browser</a>.</div></div><!-- jQuery and Boostrap --><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script></body></html>";
   var demo_html = { __html: demo_html_raw };
 
   return (
