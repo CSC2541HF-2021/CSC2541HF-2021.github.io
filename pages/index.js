9cbc355fd924fba2a15dc9bf3ad1736acf951ca0
cooper-mj <coopermj@stanford.edu>
2021-10-02 21:19:12 -0400

    Add moving location announcement; add dates to announcements

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,18 @@ export default function Home() {
           <h3>Announcements</h3>
           {/* Alert for checking out project resources. */}
           <Alert variant="info">
+            We're moving classrooms! Starting on October 4, we'll be meeting in <a href="https://goo.gl/maps/S1q3m7v3T1e76r826" target="_blank">ES B142</a> instead of our usual UC 85.
+            <br></br>
+            <div align="right">
+              <small>October 1, 2021</small>
+            </div>
+          </Alert>
+          <Alert variant="info">
             We've posted <Alert.Link href="/projectresources">project datasets and resources</Alert.Link> - make sure to check them out, as they might be helpful as you work on brainstorming for your course projects!
+            <br></br>
+            <div align="right">
+              <small>September 17, 2021</small>
+            </div>
           </Alert>
 
           <h3>Course Description</h3>
